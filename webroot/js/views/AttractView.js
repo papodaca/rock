3f4015036a41ec8a3a0228c1548a4baf1ad4b14c
Ethan Apodaca <papodaca@gmail.com>
2012-10-29 22:56:49 -0700

    Remove some testing code...

--- webroot/js/views/AttractView.js
+++ webroot/js/views/AttractView.js
@@ -4,17 +4,11 @@ define(['backbone', 'handlebars', 'jquery', 'router', 'text!views/templates/Attr
     return Backbone.View.extend({
         template: Handlebars.compile(Template),
         className: "container-fluid",
-        events: {
-            "click button": "buttonClick"
-        },
         initialize: function() {
             this.render();
         },
         render: function() {
             this.$el.append(this.template({}));
-        },
-        buttonClick: function() {
-            Router.navigate("test", {trigger: true});
         }
     });
 });
