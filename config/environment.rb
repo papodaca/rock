6dd256ef33f73655d32999adf9b0f5301fed0dcd
Ethan Apodaca <papodaca@gmail.com>
2013-05-28 19:15:33 -0700

    Add APP_CONFIG constant that will contain key values from config/config.yml

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Rock::Application.initialize!
+
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
