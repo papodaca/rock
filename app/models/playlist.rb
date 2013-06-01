082640dfa3e39fd84d591916fed30568fb5399cc
Ethan Apodaca <papodaca@gmail.com>
2013-06-01 11:49:02 -0700

    setup playlist model correctly

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,6 @@
 class Playlist < ActiveRecord::Base
   attr_accessible :name
   validates :name, :presence => true
-  has_many :songs, :through => :playlist_elements
+  has_many :playlist_elements
+  has_many :songs, :through => :playlist_elements, :order => 'playlist_elements.order'
 end
--- app/models/playlist_element.rb
+++ app/models/playlist_element.rb
@@ -1,5 +1,5 @@
 class PlaylistElement < ActiveRecord::Base
-  attr_accessible :order
+  attr_accessible :order, :playlis_id, :song_id
   validates :order, :presence => true
   belongs_to :playlist
   belongs_to :song
