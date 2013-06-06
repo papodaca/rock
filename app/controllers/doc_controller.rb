15b47b35f31e33e4bc1e3fefb409ce86ebd24dc6
Ethan Apodaca <papodaca@gmail.com>
2013-06-06 03:03:08 -0700

    working on the documentation ~50% done.

--- app/controllers/doc_controller.rb
+++ app/controllers/doc_controller.rb
@@ -51,7 +51,7 @@ class DocController < ApplicationController
       :apis => [
         {
           :path => "/sessions",
-          :description => "Create a session.",
+          :description => "Sessions with this API",
           :operations => [
             {
               :httpMethod => "POST",
@@ -60,7 +60,7 @@ class DocController < ApplicationController
               :nickname => "createSession",
               :parameters => [
                 {
-                  :description => "A json objet with user name and password.",
+                  :description => "A json objet with user name and password",
                   :paramType => "body",
                   :required => true,
                   :allowMultiple => false,
@@ -113,23 +113,24 @@ class DocController < ApplicationController
       :apis => [
         {
           :path => "/users",
-          :description => "Create a user account.",
+          :description => "Users of this API",
           :operations => [
             {
               :httpMethod => "POST",
-              :summary => "Create a user account.",
+              :summary => "Create a user account",
               :responseClass => "RegistrationResponse",
               :nickname => "createUserAccount",
               :parameters => [
                 {
-                  :description => "A json objet with user details.",
+                  :description => "A json objet with user details",
                   :paramType => "body",
                   :required => true,
                   :allowMultiple => false,
                   :dataType => "User"
                 },
                 {
-                  :description => "A password required for registration.",
+                  :name => "registration_password",
+                  :description => "A password required for registration",
                   :paramType => "query",
                   :required => false,
                   :allowMultiple => false,
@@ -199,7 +200,260 @@ class DocController < ApplicationController
       :basePath => "#{request.protocol}#{request.host_with_port}/api",
       :resourcePath => "/libraries",
       :apis => [
-      ]
+        {
+          :path => "/libraries",
+          :description => "Libraries of songs",
+          :operations => [
+            {
+              :httpMethod => "get",
+              :summary => "Current list of libraries",
+              :responseClass => "List[Library]",
+              :nickname => "listLibraries",
+              :parameters => [
+                {
+                  :name => "session_key",
+                  :description => "A session key",
+                  :paramType => "header",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "string"
+                }
+              ],
+              :errorResponses => [
+                {
+                  :code => 401,
+                  :reason => "Invalid session_key."
+                }
+              ]
+            },
+            {
+              :httpMethod => "POST",
+              :summary => "Create a library",
+              :responseClass => "Library",
+              :nickname => "createLibrary",
+              :parameters => [
+                {
+                  :description => "A Library Prototype",
+                  :paramType => "body",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "LibraryPrototype"
+                },
+                {
+                  :name => "session_key",
+                  :description => "A session key",
+                  :paramType => "header",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "string"
+                }
+              ],
+              :errorResponses => [
+                {
+                  :code => 401,
+                  :reason => "Invalid session_key."
+                }
+              ]
+            }
+          ]
+        },
+        {
+          :path => "/libraries/{id}",
+          :description => "A library of songs",
+          :operations => [
+            {
+              :httpMethod => "get",
+              :summary => "Get a library",
+              :responseClass => "Library",
+              :nickname => "getLibrary",
+              :parameters => [
+                {
+                  :name => "id",
+                  :description => "A library id",
+                  :paramType => "path",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "integer"
+                },
+                {
+                  :name => "session_key",
+                  :description => "A session key",
+                  :paramType => "header",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "string"
+                }
+              ],
+              :errorResponses => [
+                {
+                  :code => 401,
+                  :reason => "Invalid session_key."
+                },
+                {
+                  :code => 404,
+                  :reason => "Library not found"
+                }
+              ]
+            },
+            {
+              :httpMethod => "put",
+              :summary => "Update a library",
+              :responseClass => "Library",
+              :nickname => "updateLibrary",
+              :parameters => [
+                {
+                  :name => "id",
+                  :description => "A library id",
+                  :paramType => "path",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "integer"
+                },
+                {
+                  :description => "A Library Prototype",
+                  :paramType => "body",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "LibraryPrototype"
+                },
+                {
+                  :name => "session_key",
+                  :description => "A session key",
+                  :paramType => "header",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "string"
+                }
+              ],
+              :errorResponses => [
+                {
+                  :code => 401,
+                  :reason => "Invalid session_key."
+                },
+                {
+                  :code => 404,
+                  :reason => "Library not found"
+                }
+              ]
+            },
+            {
+              :httpMethod => "delete",
+              :summary => "Delete a library",
+              :responseClass => "void",
+              :nickname => "deleteLibrary",
+              :parameters => [
+                {
+                  :name => "id",
+                  :description => "A library id",
+                  :paramType => "path",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "integer"
+                },
+                {
+                  :name => "session_key",
+                  :description => "A session key",
+                  :paramType => "header",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "string"
+                }
+              ],
+              :errorResponses => [
+                {
+                  :code => 401,
+                  :reason => "Invalid session_key."
+                },
+                {
+                  :code => 404,
+                  :reason => "Library not found"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          :path => "/libraries/{id}/scan",
+          :description => "Libraries of songs",
+          :operations => [
+            {
+              :httpMethod => "post",
+              :summary => "scan a library into database",
+              :responseClass => "Library",
+              :nickname => "scanLibrary",
+              :parameters => [
+                {
+                  :name => "id",
+                  :description => "A library id",
+                  :paramType => "path",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "integer"
+                },
+                {
+                  :name => "session_key",
+                  :description => "A session key",
+                  :paramType => "header",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "string"
+                }
+              ],
+              :errorResponses => [
+                {
+                  :code => 401,
+                  :reason => "Invalid session_key."
+                },
+                {
+                  :code => 404,
+                  :reason => "Library not found"
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      :models => {
+        :Library => {
+          :id => "Library",
+          :properties => {
+            :id => {
+              :type => "integer",
+              :required => true
+            },
+            :name => {
+              :type => "string",
+              :required => true
+            },
+            :path => {
+              :type => "string",
+              :required => true
+            },
+            :songs => {
+              :type => "integer",
+              :required => true
+            },
+            :progress => {
+              :type => "integer",
+              :required => true,
+              :description => "percentage of scanning progress"
+            }
+          }
+        },
+        :LibraryPrototype => {
+          :id => "Library",
+          :properties => {
+            :name => {
+              :type => "string",
+              :required => true
+            },
+            :path => {
+              :type => "string",
+              :required => true
+            }
+          }
+        }
+      }
     }
   end
 
@@ -210,7 +464,124 @@ class DocController < ApplicationController
       :basePath => "#{request.protocol}#{request.host_with_port}/api",
       :resourcePath => "/songs",
       :apis => [
-      ]
+        {
+          :path => "/songs",
+          :description => "paginated list of songs",
+          :operations => [
+            {
+              :httpMethod => "get",
+              :summary => "paginated list of songs",
+              :responseClass => "PaginatedSongs",
+              :notes => "The actual results will varry slightly from what is shown here, try this out to see what it looks like!",
+              :nickname => "listSongs",
+              :parameters => [
+                {
+                  :name => "session_key",
+                  :description => "A session key",
+                  :paramType => "header",
+                  :required => true,
+                  :allowMultiple => false,
+                  :dataType => "string"
+                },
+                {
+                  :name => "page",
+                  :description => "page in the array",
+                  :paramType => "query",
+                  :required => false,
+                  :allowMultiple => false,
+                  :dataType => "integer"
+                },
+                {
+                  :name => "per_page",
+                  :description => "count per page",
+                  :paramType => "header",
+                  :required => false,
+                  :allowMultiple => false,
+                  :dataType => "integer"
+                }
+              ],
+              :errorResponses => [
+                {
+                  :code => 401,
+                  :reason => "Invalid session_key."
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      :models => {
+        :Song => {
+          :id => "Song",
+          :properties => {
+            :id => {
+              :type => "integer",
+              :required => true
+            },
+            :title => {
+              :type => "string",
+              :required => true
+            },
+            :track => {
+              :type => "integer",
+              :required => true
+            },
+            :length => {
+              :type => "integer",
+              :required => true
+            },
+            :artist => {
+              :type => "string",
+              :required => true
+            },
+            :album => {
+              :type => "string",
+              :required => true
+            },
+            :genre => {
+              :type => "string",
+              :required => true
+            }
+          }
+        },
+        :PaginationObject => {
+          :id => "PaginationObject",
+          :properties => {
+            :page => {
+              :type => "integer",
+              :required => true
+            },
+            :total_entries => {
+              :type => "integer",
+              :required => true
+            },
+            :total_pages => {
+              :type => "integer",
+              :required => true
+            },
+            :total_entries => {
+              :type => "integer",
+              :required => true
+            }
+          }
+        },
+        :PaginatedSongs => {
+          :id => "PaginatedSongs",
+          :properties => {
+            :page => {
+              :type => "PaginationObject",
+              :required => true,
+            },
+            :songs => {
+              :type => "Array",
+              :items => {
+                "$ref" => "Song"
+              },
+              :required => true
+            }
+          }
+        }
+      }
     }
   end
 
