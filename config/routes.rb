aff045d22deabb56f78def5201a79116b628b274
Ethan Apodaca <papodaca@gmail.com>
2013-06-06 03:09:18 -0700

    Fix up the routes to be more explict in places

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rock::Application.routes.draw do
     resources :users, :only => [:create]
     resources :sessions, :only => [:create]
     resources :libraries, :only => [:index, :create, :show, :update, :destroy] do
-      post 'scan'
+      post 'scan', :on => :member
     end
     resources :songs, :only => [:index, :show] do
-      get 'stream'
+      get 'stream', :on => :member
     end
     resources :albums, :only => [:index, :show]
     resources :genres, :only => [:index] do
@@ -16,19 +16,22 @@ Rock::Application.routes.draw do
     end
     resources :playlists, :only => [:index, :show]
     resources :artists, :only => [:index, :show] do
-      resources :songs, :only => [:index]
-      resources :albums, :only => [:index]
+      member do
+        resources :songs, :only => [:index]
+        resources :albums, :only => [:index]
+      end
     end
-    resources :doc, :only => [:index]
-    scope "doc" do
-      match 'sessions' => 'doc#sessions'
-      match 'users' => 'doc#users'
-      match 'libraries' => 'doc#libraries'
-      match 'songs' => 'doc#songs'
-      match 'albums' => 'doc#albums'
-      match 'genres' => 'doc#genres'
-      match 'playlists' => 'doc#playlists'
-      match 'artists' => 'doc#artists'
+    resources :doc, :only => [:index] do
+      collection do
+        get 'sessions'
+        get 'users'
+        get 'libraries'
+        get 'songs'
+        get 'albums'
+        get 'genres'
+        get 'playlists'
+        get 'artists'
+      end
     end
   end
 
