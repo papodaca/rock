81471cada6981a6adc78f6a24452a08a6a15da29
Ethan Apodaca <papodaca@gmail.com>
2013-06-20 01:54:50 -0700

    add capability to scan s3 buckets

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'delayed_job_active_record', '~> 0.4.4'
 gem 'daemons', '~> 1.1.9'
 gem 'mysql2', '~> 0.3.11'
 gem 'will_paginate', '~> 3.0.4'
+gem 'fog', '~> 1.12.1'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/workers/library_worker.rb
+++ app/workers/library_worker.rb
@@ -1,15 +1,32 @@
 require 'taglib'
 require 'find'
 require 'lastfm'
+require 'fog'
+
+TMP_FILE_LOCATION = APP_CONFIG["temp_dir"] + "/rockTempAudio"
 
 class LibraryWorker
 	def scan(libraryId)
 		library = Library.find(libraryId)
 		rootPath = library.data_file.path
-		return false unless File.directory?(rootPath)
 
-		lfm = Lastfm.new(APP_CONFIG['lastfm_api_key'], APP_CONFIG['lastfm_api_secret'])
+		if isS3Bucket?(rootPath)
+			puts "scan S3 bucket"
+			scanS3Bucket(rootPath, library)
+		elsif File.directory?(rootPath)
+			pust "scan local dir"
+			scanLocalDir(rootPath, library)
+		else
+			return false
+		end
 
+		puts "finished"
+		library.progress = nil
+		library.save
+	end
+	##handle_asynchronously :scan
+
+	def scanLocalDir(rootPath, library)
 		totalFilesFound = Dir[File.join(rootPath, '**', '*')].count { |f| File.file?(f) }
 		totalFilesProcessed = 0
 
@@ -19,87 +36,149 @@ class LibraryWorker
 			next if mediaType == nil
 			next if songExists?(path)
 
-			data = {}
-
-			TagLib::FileRef.open(path) do |file|
-				t = file.tag
-				p = file.audio_properties
-
-				data = { :title => t.title.to_s(),
-					:artist => t.artist.to_s(),
-					:album => t.album.to_s(),
-					:year => t.year.to_s(),
-					:track => t.track.to_s(),
-					:genre => t.genre.to_s(),
-					:length => p.length.to_s()
-				}
-			end
-
-			#get or create artist
-			artist = Artist.where(:name => data[:artist]).first
-			if artist == nil
-				artId =  getArt(lfm.artist.get_info(:artist => data[:artist])['image'])
-				artist = Artist.create(:name => data[:artist])
-				artist.art_id = artId
-				artist.save
-			end
-
-			#get or create album
-			album = Album.where(:name => data[:album], :artist_id => artist.id).first
-			if album == nil
-				artId = getArt(lfm.album.get_info(:artist => data[:artist], :album => data[:album])['image'])
-				album = Album.create(:name => data[:album])
-				album.artist_id = artist.id
-				album.art_id = artId
-				album.save
-			end
-
-			#get or create genre
-			genre = Genre.where(:name => data[:genre]).first
-			if genre == nil
-				genre = Genre.create(:name => data[:genre])
-				genre.save
-			end
-
-			#create dataFile
-			data_file = DataFile.create(:path => path)
-			data_file.media_type_id = mediaType.id
-			data_file.save
-
-			song = Song.create(:disk => 0, :length => data[:length], :title => data[:title], :track => data[:track])
-
-			song.album_id = album.id
-			song.artist_id = artist.id
-			song.genre_id = genre.id
-			song.data_file_id = data_file.id
-			song.library_id = library.id
-			song.save
+			data = getFileData(path)
+			saveSong(path, mediaType.id, library.id)
 
 			totalFilesProcessed += 1
+			updateLibrary(library, totalFilesProcessed, totalFilesFound)
+		end
+	end
+
+	def scanS3Bucket(rootPath, library)
+		connection = Fog::Storage.new(
+			:provider => 'AWS', 
+			:aws_access_key_id => APP_CONFIG['s3_key'], 
+			:aws_secret_access_key => APP_CONFIG['s3_secret'], 
+			:region => 'us-west-2')
+
+		bucket = /[S,s]3:\/\/(.+)/.match(rootPath).captures.first
+
+		puts "processing bucket: " + bucket
+
+		totalFilesFound = connection.directories.get(bucket).files.count { |f| f.content_length>0 }
+		totalFilesProcessed = 0
+
+		connection.directories.get("ea-rock-music-demo").files.each do |file|
+			next if file.content_length <= 0
+			puts "file: " + file.key
+			mediaType = getMediaType(file.key, :audio)
+			next if mediaType == nil
+			path = "S3://" + bucket + "/" + file.key
+			next if songExists?(path)
+			tmpFile = TMP_FILE_LOCATION + "." + mediaType.extension
+			
+			puts "downloading: " + file.key + " > " + tmpFile	
+			localFile = File.open(tmpFile, "wb")
+			localFile.write(file.body)
+			localFile.close
+
+			data = getFileData(tmpFile)
+			saveSong(data, path, mediaType.id, library.id)
+
+			File.delete(tmpFile)
+			totalFilesProcessed += 1
+			updateLibrary(library, totalFilesProcessed, totalFilesFound)
+		end
+	end
+
+	def updateLibrary(library, totalFilesProcessed, totalFilesFound)
 			library.progress = ((Float(totalFilesProcessed) / Float(totalFilesFound)) * 100).floor
 			library.save
-		end
-		library.progress = nil
-		library.save
 	end
-	handle_asynchronously :scan
 
+	def saveSong(data, path, mediaTypeId, libraryId)
+		lfm = Lastfm.new(APP_CONFIG['lastfm_api_key'], APP_CONFIG['lastfm_api_secret'])
+
+		#get or create artist
+		artist = Artist.where(:name => data[:artist]).first
+		if artist == nil
+			puts "creating album"
+			artId =  getArt(lfm.artist.get_info(:artist => data[:artist])['image'])
+			artist = Artist.create(:name => data[:artist])
+			artist.art_id = artId
+			artist.save
+		end
+		puts "have artist: " + artist.id.to_s()
+
+		#get or create album
+		album = Album.where(:name => data[:album], :artist_id => artist.id).first
+		if album == nil
+			puts "creating album"
+			artId = getArt(lfm.album.get_info(:artist => data[:artist], :album => data[:album])['image'])
+			album = Album.create(:name => data[:album])
+			album.artist_id = artist.id
+			album.art_id = artId
+			album.save
+		end
+		puts "have album: " + album.id.to_s()
+
+		#get or create genre
+		genre = Genre.where(:name => data[:genre]).first
+		if genre == nil
+			genre = Genre.create(:name => data[:genre])
+			genre.save
+		end
+
+		#create dataFile
+		data_file = DataFile.create(:path => path)
+		data_file.media_type_id = mediaTypeId
+		data_file.save
+
+		song = Song.create(:disk => 0, :length => data[:length], :title => data[:title], :track => data[:track])
+
+		song.album_id = album.id
+		song.artist_id = artist.id
+		song.genre_id = genre.id
+		song.data_file_id = data_file.id
+		song.library_id = libraryId
+		song.save
+	end
+
+	def getFileData(path)
+		data = {}
+		TagLib::FileRef.open(path) do |file|
+			t = file.tag
+			p = file.audio_properties
+
+			data = { :title => t.title.to_s(),
+				:artist => t.artist.to_s(),
+				:album => t.album.to_s(),
+				:year => t.year.to_s(),
+				:track => t.track.to_s(),
+				:genre => t.genre.to_s(),
+				:length => p.length.to_s()
+			}
+		end
+		return data
+	end
+
+	def isS3Bucket?(path)
+		/[s,S]3:\/\/.+$/.match(path) != nil
+	end
 	def getMediaType(path, selector)
 		ext = getExtension(path)
 		return nil if ext == nil or ext == ""
 		return MediaType.where(selector => true, :extension => ext).first
 	end
 	def getExtension(path)
-		return /\.([0-9a-zA-Z]+)$/.match(path).captures.first
+		matches = /\.([0-9a-zA-Z]+)$/.match(path)
+		return matches.captures.first if matches != nil
+		return nil
 	end
 	def getArt(images)
 		begin
 			art = images[images.index { |x| x['size'] == "large"}]['content']
 			dataArt = DataFile.create(:path => art)
-			dataArt.media_type_id = getMediaType(art, :image).id
+			mediaType = getMediaType(art, :image)
+			return nil if mediaType == nil
+			dataArt.media_type_id = mediaType.id
 			dataArt.save
 			art = Art.create(:data_file_id => dataArt.id)
-			return art.id
+			if art != nil
+				return art.id
+			else
+				return nil
+			end
 		rescue Lastfm::ApiError
 			return nil
 		end
