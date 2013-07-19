eb74ca3b657755f1520242cabea88e1db4445999
Ethan Apodaca <papodaca@gmail.com>
2013-07-19 13:20:07 -0700

    order albums songs by track #

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,7 @@ class Album < ActiveRecord::Base
   validates :name, :presence=> true
   belongs_to :art
   belongs_to :artist
-  has_many :songs
+  has_many :songs, -> { order("track asc") }
 
   def length
     self.songs.sum(:length)
